2fc80ae9ab8a23c8b7e0cf28755215087e292f48
Jeff Allen <1063272+jeffallen6767@users.noreply.github.com>
2017-09-02 19:46:37 -0700

    use bignumber, add real tests

--- src/finite-field.js
+++ src/finite-field.js
@@ -1,44 +1,132 @@
 // finite field, see: https://en.wikipedia.org/wiki/Finite_field
-// this is an exercise from https://eng.paxos.com/blockchain-101-foundational-math
-
-// Write a program to calculate 1*k, 2*k, 3*k, … 30*k for some k in F31. 
-// Notice anything about these sets?
+// note: in order to do this with arbitrarily large numbers
+// without rounding error we have to use bignumber
+// NOTE: bignumbers are prefixed with b_
 
 var 
+  BigNumber = require('bignumber.js'),
   CHAR_ONE_SPACE = " ",
   CHAR_NEW_LINE = "\n";
 
 function range(start, end, step) {
   var 
     result = [],
-    amount = step || 1,
-    idx = 0,
-    x;
+    b_idx = new BigNumber(0),
+    b_start = new BigNumber(start),
+    b_end = new BigNumber(end),
+    b_amount = new BigNumber(step || 1);
   
-  for (x=start; x<end; x+=amount) {
-    result[idx++] = x;
+  while (b_start.lt(b_end)) {
+    var
+      idx = b_idx.valueOf();
+    result[idx] = new BigNumber(b_start);
+    b_start = b_start.add(b_amount);
+    b_idx = b_idx.add(1);
   }
-  
+
   return result;
 }
 
 function finite_field(order) {
   var 
-    field = range(1, order),
-    result = {
-      "order": order,
+    minimum = new BigNumber(0),
+    maximum = new BigNumber(order),
+    field = range(minimum, maximum),
+    math_funcs = {
+      "add": function (b_x, b_y, b_o) {
+        /*
+        return (x + y) % order;
+        */
+        var 
+          b_s = b_x.add(b_y),
+          b_r = b_s.mod(b_o);
+        return b_r;
+      },
+      "subtract": function (b_x, b_y, b_o) {
+        /*
+        return (x - y) % order;
+        */
+        var 
+          b_s = b_x.sub(b_y),
+          b_r = b_s.lt(minimum) ? b_s.add(b_o) : b_s;
+        return b_r;
+      },
+      "multiply": function (b_x, b_y, b_o) {
+        /*
+        return (x * y) % order;
+        */
+        var 
+          b_s = b_x.mul(b_y),
+          b_r =  b_s.mod(b_o);
+        return b_r;
+      },
+      "divide": function (b_x, b_y, b_o) {
+        /*
+        return (x / y) % order;
+        note: using Fermat’s Little Theorem.
+        n^(p-1)=1 mod p where p is prime
+        which implies
+        n^(p-2)=1/n mod p where p is prime
+        2/3 = 2 * (3 ^ (p-2)) = 4 mod p where p is prime
+        */
+        var 
+          b_p = b_o.sub(2),
+          b_s = b_x.mul(
+            b_y.pow(b_p)
+          ),
+          b_r = b_s.mod(b_o);
+        return b_r;
+      },
+      "pow": function (b_x, b_y, b_o) {
+        /*
+        return (x ^ y) % order;
+        */
+        var 
+          b_r =  b_x.pow(b_y, b_o);
+        return b_r;
+      }
+    },
+    math_keys = Object.keys(math_funcs),
+    finite_field = {
+      "minimum": minimum,
+      "maximum": maximum,
+      "order": maximum,
       "field": field,
+      "data": {},
+      "math": {},
       "toString": function() {
         return [
           "finite field order:",
           order,
           "=",
-          "[" + field[0] + ", " + field[1] + ", ..." + field[order-2] + "]"
+          "[" + field[0] + ", " + field[1] + ", ..." + field[order-1] + "]"
         ].join(CHAR_ONE_SPACE);
       }
     };
   
-  return result;
+  // create math functions:
+  math_keys.forEach(function(math_key) {
+    var
+      math = finite_field.math,
+      data = finite_field.data,
+      math_func = math_funcs[math_key];
+    math[math_key] = function(b_x, b_y) {
+      var
+        result = data[math_key];
+      if (!result) {
+        result = data[math_key] = calc_sets(finite_field, math_func);
+      }
+      if (b_x) {
+        result = result.sets[b_x];
+        if (b_y) {
+          result = result[b_y];
+        }
+      }
+      return result;
+    };
+  });
+  
+  return finite_field;
 }
 
 function calc_sets(ff, algo) {
@@ -46,19 +134,25 @@ function calc_sets(ff, algo) {
     sets = [],
     field = ff.field,
     order = ff.order,
-    start = 0,
-    end = field.length,
+    b_x = new BigNumber(0),
+    b_y = new BigNumber(0),
+    b_max = new BigNumber(field.length),
     HORZ_RULE = [],
     x,y,z,
     result;
   
-  if (end) {
-    for (x=start; x<end; x++) {
+  if (b_max.gt(0)) {
+    while (b_x.lt(b_max)) {
+      x = b_x.valueOf();
       sets[x] = z = [];
       HORZ_RULE.push("---");
-      for (y=start; y<end; y++) {
+      b_y = new BigNumber(0);
+      while (b_y.lt(b_max)) {
+        y = b_y.valueOf();
         z[y] = algo(field[x], field[y], order);
+        b_y = b_y.add(1);
       }
+      b_x = b_x.add(1);
     }
   }
   
@@ -72,7 +166,7 @@ function calc_sets(ff, algo) {
         lines = ruled.map(function(set, idx) {
           var 
             num = idx - 1,
-            vert = (num > 0 ? [num] : []).concat(set);
+            vert = (num > 0 ? [num-1] : []).concat(set);
           return vert.reduce(function(sum, val, index) {
             sum.push(
               (val < 10 ? CHAR_ONE_SPACE + val : val) + (num < 1 || index ? "" : "|")
--- test.js
+++ test.js
@@ -1,116 +1,331 @@
 // test finite field
 var
-  debug = false,
-  FINITE_FIELD_ORDER = 31,
   tester = require("testing"),
+  BigNumber = require('bignumber.js'),
+  FINITE_FIELD_ORDER = new BigNumber(31),
   ff = require("./index"),
   CHAR_ONE_SPACE = " ",
   field = ff.finite_field(FINITE_FIELD_ORDER),
-  min = field.field[0],
-  test = debug ? {"x": 4, "y": 8, "out": []} : null,
-  sets = [
-    {
-      "title": "add", 
-      "data": ff.calc_sets(field, function (x, y, order) {
-        /*
-        return (x + y) % order;
-        */
-        var simple = x + y,
-          result = simple > order ? simple % order : simple;
-        if (test && x == test.x && y == test.y) {
-          test.out.push({
-            "add": x + "+" + y, 
-            "x": x, 
-            "y": y,
-            "simple": simple,
-            "result": result
-          });
-        }
-        return result;
-      })
+  field_name = field.toString(),
+  reducer = function(obj) {
+    return obj.reduce(function(sum, val) {
+      sum.push(
+        val.valueOf()
+      );
+      return sum;
+    }, []);
+  },
+  testData = {
+    "add": {
+      "title": [
+        "add x + y mod p for all k",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["add"],
+          data = func(),
+          sets = data.sets,
+          order = FINITE_FIELD_ORDER.toNumber(),
+          num_rows = sets.length,
+          row = sets[17],
+          num_cols = row.length,
+          vals = reducer(row),
+          expected = "17,18,19,20,21,22,23,24,25,26,27,28,29,30,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16",
+          getRow = func(17),
+          getRowLen = getRow.length,
+          getReduced = reducer(getRow),
+          getEntry = func(17,27),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log(data.toString());
+        console.log("test number of rows");
+        test.assert.identical(num_rows, order);
+        console.log("test number of columns");
+        test.assert.identical(num_cols, order);
+        console.log("test values in row 2");
+        test.assert.identical(vals.join(), expected);
+        console.log("test number of entries in getRow(2)");
+        test.assert.identical(getRowLen, order);
+        console.log("test values of entries in getRow(2)");
+        test.assert.identical(getReduced.join(), expected);
+        console.log("test value of 17 + 27 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 13);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
     },
-    {
-      "title": "subtract", 
-      "data": ff.calc_sets(field, function (x, y, order) {
-        /*
-        var r = x - y,
-          t = r < 1 ? order + r : r;
-        return t % order;
-        */
-        var simple = x - y,
-          result = simple < min ? order + simple : simple;
-        if (test && x == test.x && y == test.y) {
-          test.out.push({
-            "subtract": x + "-" + y, 
-            "x": x, 
-            "y": y,
-            "simple": simple,
-            "result": result
-          });
-        }
-        return result;
-      })
+    "subtract": {
+      "title": [
+        "subtract x - y mod p for all k",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["subtract"],
+          data = func(),
+          sets = data.sets,
+          order = FINITE_FIELD_ORDER.toNumber(),
+          num_rows = sets.length,
+          row = sets[17],
+          num_cols = row.length,
+          vals = reducer(row),
+          expected = "17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,30,29,28,27,26,25,24,23,22,21,20,19,18",
+          getRow = func(17),
+          getRowLen = getRow.length,
+          getReduced = reducer(getRow),
+          getEntry = func(17,27),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log(data.toString());
+        console.log("test number of rows");
+        test.assert.identical(num_rows, order);
+        console.log("test number of columns");
+        test.assert.identical(num_cols, order);
+        console.log("test values in row 2");
+        test.assert.identical(vals.join(), expected);
+        console.log("test number of entries in getRow(2)");
+        test.assert.identical(getRowLen, order);
+        console.log("test values of entries in getRow(2)");
+        test.assert.identical(getReduced.join(), expected);
+        console.log("test value of 17 - 27 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 21);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
     },
-    {
-      "title": "multiply", 
-      "data": ff.calc_sets(field, function (x, y, order) {
-        /*
-        return (x * y) % order;
-        */
-        var simple = x * y,
-          result = simple > order ? simple % order : simple;
-        if (test && x == test.x && y == test.y) {
-          test.out.push({
-            "multiply": x + "*" + y, 
-            "x": x, 
-            "y": y,
-            "simple": simple,
-            "result": result
-          });
-        }
-        return result;
-      })
+    "multiply": {
+      "title": [
+        "multiply x * y mod p for all k",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["multiply"],
+          data = func(),
+          sets = data.sets,
+          order = FINITE_FIELD_ORDER.toNumber(),
+          num_rows = sets.length,
+          row = sets[17],
+          num_cols = row.length,
+          vals = reducer(row),
+          expected = "0,17,3,20,6,23,9,26,12,29,15,1,18,4,21,7,24,10,27,13,30,16,2,19,5,22,8,25,11,28,14",
+          getRow = func(17),
+          getRowLen = getRow.length,
+          getReduced = reducer(getRow),
+          getEntry = func(17,27),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log(data.toString());
+        console.log("test number of rows");
+        test.assert.identical(num_rows, order);
+        console.log("test number of columns");
+        test.assert.identical(num_cols, order);
+        console.log("test values in row 2");
+        test.assert.identical(vals.join(), expected);
+        console.log("test number of entries in getRow(2)");
+        test.assert.identical(getRowLen, order);
+        console.log("test values of entries in getRow(2)");
+        test.assert.identical(getReduced.join(), expected);
+        console.log("test value of 17 * 27 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 25);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
     },
-    {
-      "title": "divide", 
-      "data": ff.calc_sets(field, function (x, y, order) {
-        // very inefficient for larger fields
-        var t = x,
-          result = simple = t / y,
-          tries = test && [[t, result]];
-        while (result !== Math.floor(result)) {
-          t += order;
-          result = t / y;
-          test && tries.push([t, result]);
-        }
-        if (test && x == 29 && y == 27) {
-          test.out.push({
-            "divide": x + "/" + y, 
-            "x": x, 
-            "y": y,
-            "t": t,
-            "tries": tries,
-            "simple": simple,
-            "result": result
-          });
-        }
-        return result;
-      })
+    "divide": {
+      "title": [
+        "divide x / y mod p for all k",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["divide"],
+          data = func(),
+          sets = data.sets,
+          order = FINITE_FIELD_ORDER.toNumber(),
+          num_rows = sets.length,
+          row = sets[17],
+          num_cols = row.length,
+          vals = reducer(row),
+          expected = "0,17,24,16,12,22,8,29,6,26,11,10,4,18,30,28,3,1,13,27,21,20,5,25,2,23,9,19,15,7,14",
+          getRow = func(17),
+          getRowLen = getRow.length,
+          getReduced = reducer(getRow),
+          getEntry = func(17,27),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log(data.toString());
+        console.log("test number of rows");
+        test.assert.identical(num_rows, order);
+        console.log("test number of columns");
+        test.assert.identical(num_cols, order);
+        console.log("test values in row 2");
+        test.assert.identical(vals.join(), expected);
+        console.log("test number of entries in getRow(2)");
+        test.assert.identical(getRowLen, order);
+        console.log("test values of entries in getRow(2)");
+        test.assert.identical(getReduced.join(), expected);
+        console.log("test value of 17 / 27 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 19);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
+    },
+    "pow": {
+      "title": [
+        "pow x ^ y mod p for all k",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["pow"],
+          data = func(),
+          sets = data.sets,
+          order = FINITE_FIELD_ORDER.toNumber(),
+          num_rows = sets.length,
+          row = sets[17],
+          num_cols = row.length,
+          vals = reducer(row),
+          expected = "1,17,10,15,7,26,8,12,18,27,25,22,2,3,20,30,14,21,16,24,5,23,19,13,4,6,9,29,28,11,1",
+          getRow = func(17),
+          getRowLen = getRow.length,
+          getReduced = reducer(getRow),
+          getEntry = func(17,27),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log(data.toString());
+        console.log("test number of rows");
+        test.assert.identical(num_rows, order);
+        console.log("test number of columns");
+        test.assert.identical(num_cols, order);
+        console.log("test values in row 2");
+        test.assert.identical(vals.join(), expected);
+        console.log("test number of entries in getRow(2)");
+        test.assert.identical(getRowLen, order);
+        console.log("test values of entries in getRow(2)");
+        test.assert.identical(getReduced.join(), expected);
+        console.log("test value of 17 ^ 27 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 29);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
+    },
+    "finite_field.math.multiply(x,y)": {
+      "title": [
+        "compute 24 * 19 % 31",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["multiply"],
+          getEntry = func(24,19),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log("test value of 24 * 19 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 22);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
+    },
+    "finite_field.math.pow(x,y)": {
+      "title": [
+        "compute 17 ^ 3 % 31",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          func = math["pow"],
+          getEntry = func(17,3),
+          getEntryVal = getEntry.toNumber();
+        test.endTime();
+        // output
+        console.log("test value of 17 ^ 3 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 15);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
+    },
+    "finite_field.math.pow(x,y) and finite_field.math.multiply(x,y)": {
+      "title": [
+        "compute 5 ^ 5 * 18 % 31",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          pow = math["pow"],
+          getEntryOne = pow(5,5),
+          mult = math["multiply"],
+          getEntryTwo = mult(getEntryOne, 18),
+          getEntryVal = getEntryTwo.toNumber();
+        test.endTime();
+        // output
+        console.log("test value of 5 ^ 5 * 18 % 31 using getEntry");
+        test.assert.identical(getEntryVal, 16);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
+    },
+    "finite_field.math.pow(x,y) k^30 % 31 for all k": {
+      "title": [
+        "k^30 % 31 for all k ( except zero ) in",
+        field_name
+      ].join(" in "),
+      "tests": function(test) {
+        test.startTime();
+        var
+          math = field.math,
+          pow = math["pow"],
+          row = field.field.reduce(function(sum, val, idx) {
+            if (idx) {
+              sum.push(
+                val.pow(30, FINITE_FIELD_ORDER)
+              );
+            }
+            return sum;
+          }, []),
+          vals = reducer(row),
+          expected = "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1";
+        test.endTime();
+        // output
+        console.log("test values in row 30");
+        test.assert.identical(vals.join(), expected);
+        console.log(CHAR_ONE_SPACE);
+        test.done();
+      }
     }
-  ];
+  },
+  testKeys = Object.keys(testData),
+  tests = {};
 
-console.log(CHAR_ONE_SPACE);
-console.log(field.toString());
-
-sets.forEach(function(set) {
-  console.log(CHAR_ONE_SPACE);
-  console.log(set.title);
-  console.log(set.data.toString());
+// make tests
+testKeys.forEach(function(testKey) {
+  var
+    testType = testData[testKey];
+  tests[testType.title] = testType.tests;
 });
 
-if (test) {
-  console.log(CHAR_ONE_SPACE);
-  test.out.forEach(function(row) {
-    console.log(row);
-  });
-}
+// run tests
+tester.run(tests);
